f73ecdc02c49e0c54fdcb65957568551f71adfb6
psampathreddy5 <sampath4721.sam@gmail.com>
2023-08-08 22:46:56 +0530

    latest changes v1.0.1

---
+++ pods/init-container/nginx.sh
@@ -0,0 +1,8 @@
+apt-get update
+apt-get install net-tools -y
+apt-get install net-tools sudo -y
+sudo apt-get update
+sudo apt-get install vim -y
+sudo apt-get install nano -y
+sudo apt-get install openssh-client -y
+sudo apt install openssh-server -y
